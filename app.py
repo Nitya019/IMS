43e217b99aaa06ba24aa50bd2bb50b76a3732161
Nitya019 <vrmnitya19@gmail.com>
2025-06-10 17:16:53 +0530

    add more pages

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-#initiating
+import streamlit as st
+
+
+
+# Initialize session state variables
+if 'page' not in st.session_state:
+    st.session_state.page = 'home'
+
+# Page switch function
+def go_to(page_name):
+    st.session_state.page = page_name
+
+# --- Navigation ---
+st.markdown("""
+<style>
+.big-font {
+    font-size:75px !important;
+    color:white;
+    text-align: center;
+    font-family: 'Roboto', Monospace;
+}
+</style>
+<div class="big-font">VN JEWELLERS</div>
+""", unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("Earrings"):
+        go_to('earrings')
+
+with col2:
+    if st.button("Set"):
+        go_to('set')
+
+st.markdown("---")
+
---
+++ page_set.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+import pandas as pd 
+import os
+
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:75px !important;
+        color:white;
+        text-align: center;
+        font-family: 'Roboto', Monospace; 
+    }
+    </style>
+    <div class="big-font">VN JEWELLERS<div>
+    """, unsafe_allow_html=True)
+
+st.header("SET COLLECTION")
+
+
+def display_images(folder):
+    image_files = os.listdir(folder)
+    cols = st.columns(3)
+    for idx, image in enumerate(image_files):
+        with cols[idx % 3]:
+            st.image(os.path.join(folder, image), use_container_width=True)
+
+# --- Display earring images ---
+display_images("images/set")
